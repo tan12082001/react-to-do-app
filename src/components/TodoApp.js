06b87f8fce5cb3adfc2555e6f0fc5fa4e7634575
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-15 16:21:42 +0530

    Add components for the TodoList App

---
+++ src/components/TodoApp.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import TodosLogic from './todosLogic';
+
+const TodoApp = () => (
+  <div className="wrapper">
+    <div className="todos">
+      <Header />
+      <TodosLogic />
+    </div>
+  </div>
+);
+export default TodoApp;
